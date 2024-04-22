01a4b5511dbdc3624044c95e8ad5abd04570a2eb
Ilya Kovalkov <ilya.kovalkov@ferendin.com>
2024-04-22 12:05:33 +0200

    Prototype message handling

--- src/main/kotlin/net/jakot/appoin/appointmentnotifier/telegram/TelegramBot.kt
+++ src/main/kotlin/net/jakot/appoin/appointmentnotifier/telegram/TelegramBot.kt
@@ -1,17 +1,22 @@
 package net.jakot.appoin.appointmentnotifier.telegram
 
+import jakarta.annotation.PostConstruct
 import mu.KotlinLogging
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
 
-class TelegramBot(botToken: String, private val botName: String) : TelegramLongPollingBot(botToken) {
+class TelegramBot(botToken: String, private val botName: String, private val router: TelegramBotRouter) : TelegramLongPollingBot(botToken) {
 
     private val log = KotlinLogging.logger {}
     override fun getBotUsername(): String {
         return botName
     }
 
+    @PostConstruct
+
     override fun onUpdateReceived(update: Update?) {
-        log.info { "Received update: ${update?.message?.text}" }
+        update?.let {
+            router.route(it)
+        }
     }
 }
